90fcd9a0a4f51738ce06fa3082b5af1795980494
evan <evan@evanw.org>
2023-03-02 15:39:23 -0600

    add cleaned get_lightcurve function

---
+++ get_lightcurve.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+import lightkurve as lk
+
+def threshold_positive(x):
+    """Find outliers and replace them with samples from a Gaussian"""
+    data = np.copy(x)
+    std = np.nanstd(data)
+    absdiff = np.abs(np.diff(data, prepend=data[0]))
+    thresh = 3 * std
+
+    outlier = np.zeros(len(data), dtype=bool)
+    outlier[data > thresh] = True
+    outlier[absdiff > thresh] = True
+
+    # replace outliers with randomly sampled values
+    std_inliers = np.nanstd(data[~outlier])
+    data[outlier] = np.random.normal(0, std_inliers, size=outlier.sum())
+
+    return data
+
+def get_lightcurve(test=False):
+    """Return a lightcurve and its associated data
+
+    Args:
+        test (bool): always return the same lightcurve so it can be cached by
+            lightkurve
+
+    Returns:
+        lc (pandas DataFrame): lightcurve information in a pandas dataframe.
+            The relevant columns are 'flux_clean' and 'transit_hours
+        lc_info (pandas Series): contains information about the specific
+            lightcurve such as Kepler ID ('kepid'), orbital period ('koi_period'),
+            transit duration ('koi_duration'), planet/star ratio ('koi_ror'),
+    """
+
+    lc_sample_time = 29.424
+    BKJD_start_time = 2454833 # offset of Kepler start time in BJD
+    data = pd.read_csv('cumulative_2023.02.25_10.21.50.tab', sep='\t', dtype={'koi_quarters': str})
+    data = data.dropna()
+    data['num_quarters'] = data.koi_quarters.apply(lambda x: sum(map(int, list(x))))
+
+    # filter out bad lightcurve candidates
+    data = data[
+        (data.koi_period > 4) &
+        (data.koi_period < 12) &
+        (data.koi_prad > 3) &
+        (data.num_quarters>12)
+    ]
+    x = pd.read_pickle('/tmp/selected_curve.pkl')
+
+    # choose a lightcurve
+    if test:
+        lc_info = data.iloc[0]
+    else:
+        lc_info = data.sample().squeeze()
+
+    # download data for lightcurve and optionally cache it
+    lc_files = lk.search_lightcurve(
+        f"KIC {lc_info.kepid}", author='Kepler', cadence = "long"
+    ).download_all(download_dir='.' if test else None)
+
+    lc_chunks = []
+    for lcf in lc_files:
+        lc_chunk = lcf.to_pandas()
+        median = np.nanmedian(lc_chunk.flux)
+        lc_chunk['flux_clean'] = (threshold_positive(lc_chunk.flux - median) + median) / median
+        lc_chunks.append(lc_chunk)
+
+    # stitch lightcurve data and filter out low quality data points
+    lc = pd.concat(lc_chunks).query('quality == 0')
+
+    # # phase fold data points
+    lc['folded_time'] = (
+        lc.index - lc_info.koi_time0bk - lc_info.koi_period / 2
+    ) % lc_info.koi_period
+    lc = lc.sort_values('folded_time')
+
+    # # grab actual transit part of data
+    window = 8 * lc_info.koi_duration / 24
+    start = lc_info.koi_period / 2 - window / 2
+    end = lc_info.koi_period / 2 + window / 2
+    lc = lc[(lc.folded_time > start) & (lc.folded_time < end)]
+    lc['transit_hours'] = (lc.folded_time - lc_info.koi_period / 2) * 24
+
+    return lc, lc_info
